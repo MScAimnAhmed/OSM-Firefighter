c0148c69856ec822b4b2342081f3774b0fec84db
Samuel Holderbach <st152101@stud.uni-stuttgart.de>
2021-07-28 15:13:02 +0200

    implemented server base

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,26 @@
+use actix_web::{get, HttpServer, App, Responder, HttpResponse};
 use crate::graph::Graph;
 
 mod graph;
 
-fn main() {
-    let graph = Graph::from_file("resources/toy.fmi");
-    println!("{:#?}", graph);
+/// Request to check whether the server is up and available
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let _graph = Graph::from_file("resources/toy.fmi");
+    println!("Parsed graph toy.fmi");
+    //println!("{:#?}", _graph);
+
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(ping)
+    });
+    println!("Starting web server on 127.0.0.1:8080");
+    server.bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
