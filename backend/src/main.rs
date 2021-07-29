8846a54f0edf1ea5da31cdf67e187682bbe0de26
Samuel Holderbach <st152101@stud.uni-stuttgart.de>
2021-07-29 02:05:03 +0200

    implemented handler to display list of graphs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,11 @@
+use std::fs;
+
 use actix_web::{get, HttpServer, App, Responder, HttpResponse};
-use crate::graph::Graph;
+use serde_json::json;
 
 mod graph;
+
+//use crate::graph::Graph;
 
 /// Request to check whether the server is up and available
 #[get("/ping")]
@@ -9,15 +13,43 @@ async fn ping() -> impl Responder {
     HttpResponse::Ok().body("pong")
 }
 
+/// List all graph files that can be parsed by the server
+#[get("/")]
+async fn list_graphs() -> impl Responder {
+    match fs::read_dir("resources/") {
+        Ok(paths) => {
+            let mut graphs = Vec::new();
+            for path in paths {
+                let path = path.unwrap();
+                let filetype = path.file_type().unwrap();
+                if filetype.is_dir() {
+                    continue;
+                }
+                let filename = String::from(
+                    path.file_name()
+                        .to_str()
+                        .unwrap()
+                );
+                if !filename.ends_with(".fmi") {
+                    continue;
+                }
+                graphs.push(filename);
+            }
+            HttpResponse::Ok().json(json!(graphs))
+        },
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string())
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let _graph = Graph::from_file("resources/toy.fmi");
-    println!("Parsed graph toy.fmi");
-    //println!("{:#?}", _graph);
+    //let graph = Graph::from_file("resources/toy.fmi");
+    //println!("{:#?}", graph);
 
     let server = HttpServer::new(|| {
         App::new()
             .service(ping)
+            .service(list_graphs)
     });
     println!("Starting web server on 127.0.0.1:8080");
     server.bind("127.0.0.1:8080")?
