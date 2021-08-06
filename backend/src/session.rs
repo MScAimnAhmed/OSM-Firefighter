c6f0e05fa4f3e35a6d0639e3cb120796f9b9372f
Samuel Holderbach <st152101@stud.uni-stuttgart.de>
2021-08-06 21:50:16 +0200

    sessions need to be pruned manually

--- backend/src/session.rs
+++ backend/src/session.rs
@@ -1,7 +1,10 @@
+use std::time::{Instant, Duration};
+
 use actix_web::http::Cookie;
 use actix_web::cookie::SameSite;
 use nanoid;
 use transient_hashmap::TransientHashMap;
+use std::ops::Sub;
 
 /// Container for OSM-Firefighter session data
 pub struct OSMFSession {
@@ -25,22 +28,33 @@ impl OSMFSession {
 }
 
 /// Time, after which to prune unused `OSMFSession` instances
-const PRUNE_SESSIONS_AFTER_SECS: u32 = 60 * 60;
+const PRUNE_SESSIONS_AFTER_SECS: Duration = Duration::from_secs(60 * 60);
 
 /// Storage for `OSMFSession` instances
 pub struct OSMFSessionStorage {
     sessions: TransientHashMap<String, OSMFSession>,
+    last_pruned: Instant,
 }
 impl OSMFSessionStorage {
     /// Create a new storage for `OSMFSession` instances
     pub fn new() -> Self {
         Self {
-            sessions: TransientHashMap::new(PRUNE_SESSIONS_AFTER_SECS),
+            sessions: TransientHashMap::new(PRUNE_SESSIONS_AFTER_SECS.as_secs() as u32),
+            last_pruned: Instant::now(),
+        }
+    }
+
+    /// Prune unused `OSMFSession` instances
+    fn prune_sessions(&mut self) {
+        if self.last_pruned.elapsed() >= PRUNE_SESSIONS_AFTER_SECS {
+            self.sessions.prune();
+            self.last_pruned = Instant::now();
         }
     }
 
     /// Open a new `OSMFSession`
     pub fn open_session(&mut self) -> Cookie {
+        self.prune_sessions();
         let session = OSMFSession::new(nanoid::nanoid!());
         let cookie = session.build_cookie();
         self.sessions.insert(session.id.clone(), session);
@@ -49,6 +63,7 @@ impl OSMFSessionStorage {
 
     /// Refresh the `OSMFSession` with session id `id`
     pub fn refresh_session(&mut self, id: &str) -> Option<Cookie> {
+        self.prune_sessions();
         let string_id = &id.to_string();
         if self.sessions.contains_key(string_id) {
             None
@@ -59,6 +74,7 @@ impl OSMFSessionStorage {
 
     /// Get a mutable reference to the `OSMFSession` with session id `id`
     pub fn get_mut_session(&mut self, id: &str) -> Option<&mut OSMFSession> {
+        self.prune_sessions();
         let string_id = &id.to_string();
         self.sessions.get_mut(string_id)
     }
