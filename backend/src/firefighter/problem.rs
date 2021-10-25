7b05b33f6b0484b1ac9af951ada76e14643d242b
Samuel Holderbach <st152101@stud.uni-stuttgart.de>
2021-10-25 23:19:22 +0200

    reworked implementation of node data storage

--- backend/src/firefighter/problem.rs
+++ backend/src/firefighter/problem.rs
@@ -1,5 +1,5 @@
 use std::{collections::BTreeMap,
-          fmt::Formatter,
+          // fmt::Formatter,
           sync::{Arc, RwLock}};
 
 use log;
@@ -12,12 +12,6 @@ use crate::graph::Graph;
 /// `u64` type alias to denote a time unit in the firefighter problem
 pub type TimeUnit = u64;
 
-/// State of a node in the firefighter problem
-#[derive(Debug, Serialize)]
-pub enum NodeState {
-    Burning,
-    Defended,
-}
 /// Settings for a firefighter problem instance
 #[derive(Debug)]
 pub struct OSMFSettings {
@@ -39,82 +33,90 @@ impl OSMFSettings {
 #[derive(Debug, Serialize)]
 pub struct NodeData {
     pub node_id: usize,
-    state: NodeState,
     time: TimeUnit,
 }
 
-impl NodeData {
-    /// Create new node data with state `state` for node with id `node_id`
-    fn new(node_id: usize, state: NodeState, time: TimeUnit) -> Self {
-        Self {
-            node_id,
-            state,
-            time,
-        }
-    }
-
-    /// Is corresponding node burning?
-    pub fn is_burning(&self) -> bool {
-        matches!(self.state, NodeState::Burning)
-    }
-
-    /// Is corresponding node defended?
-    pub fn is_defended(&self) -> bool {
-        matches!(self.state, NodeState::Defended)
-    }
-}
-
 /// Storage for node data
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct NodeDataStorage {
-    storage: BTreeMap<usize, NodeData>,
+    burning: BTreeMap<usize, NodeData>,
+    defended: BTreeMap<usize, NodeData>,
+    times: BTreeMap<TimeUnit, Vec<usize>>,
 }
 
 impl NodeDataStorage {
     /// Create a new node data storage
     fn new() -> Self {
         Self {
-            storage: BTreeMap::new(),
+            burning: BTreeMap::new(),
+            defended: BTreeMap::new(),
+            times: BTreeMap::new(),
         }
     }
 
-    /// Is node data attached to node with id `node_id`?
-    pub fn is_node_data_attached(&self, node_id: &usize) -> bool {
-        self.storage.contains_key(node_id)
+    /// Is node with id `node_id` burning?
+    fn is_burning(&self, node_id: &usize) -> bool {
+        self.burning.contains_key(node_id)
     }
 
-    /// Attach new node data to the node with id `node_id`
-    pub fn attach_node_data(&mut self, node_id: usize, state: NodeState, time: TimeUnit) {
-        self.storage.insert(node_id, NodeData::new(node_id, state, time));
+    /// Is node with id `node_id` defended?
+    fn is_defended(&self, node_id: &usize) -> bool {
+        self.defended.contains_key(node_id)
     }
 
-    /// Try to attach new node data to the node with id `node_id`.
-    /// Return an error if node data is already attached to the node.
-    pub fn try_attach_node_data(&mut self, node_id: usize, state: NodeState, time: TimeUnit) -> Result<(), OSMFProblemError> {
-        if !self.is_node_data_attached(&node_id) {
-            self.attach_node_data(node_id, state, time);
-            Ok(())
-        } else {
-            Err(OSMFProblemError::NodeDataAlreadyAttached)
+    /// Is node with id `node_id` undefended?
+    pub fn is_undefended(&self, node_id: &usize) -> bool {
+        !(self.is_burning(node_id) || self.is_defended(node_id))
+    }
+
+    /// Update `self.times` for given time with given nodes
+    fn update_times(&mut self, time: TimeUnit, updated: Vec<usize>) {
+        self.times.entry(time)
+            .and_modify(|nodes| {
+                nodes.reserve(updated.len());
+                for node_id in &updated {
+                    nodes.push(*node_id);
+                }
+            })
+            .or_insert(updated);
+    }
+
+    /// Mark all nodes in `nodes` as burning at time `time`
+    pub fn mark_burning(&mut self, nodes: Vec<usize>, time: TimeUnit) {
+        for node_id in &nodes {
+            self.burning.insert(*node_id, NodeData {
+                node_id: *node_id,
+                time,
+            });
         }
+        self.update_times(time, nodes);
+    }
+
+    /// Mark all nodes in `nodes` as defended at time `time`
+    pub fn mark_defended(&mut self, nodes: Vec<usize>, time: TimeUnit) {
+        for node_id in &nodes {
+            self.defended.insert(*node_id, NodeData {
+                node_id: *node_id,
+                time,
+            });
+        }
+        self.update_times(time, nodes);
     }
 
     /// Get the node data of all burning vertices
-    pub fn get_all_burning(&self) -> Vec<&NodeData> {
-        self.storage.values()
-            .filter(|&nd| nd.is_burning())
-            .collect::<Vec<_>>()
+    pub fn get_burning(&self) -> Vec<&NodeData> {
+        self.burning.values().collect()
     }
 }
 
 /// Container for data about the simulation of a firefighter problem instance
 #[derive(Serialize)]
 pub struct OSMFSimulationResponse<'a> {
-    node_data: &'a BTreeMap<usize, NodeData>,
     nodes_burned: usize,
     nodes_defended: usize,
     nodes_total: usize,
     end_time: TimeUnit,
+    node_data: &'a NodeDataStorage,
 }
 
 /// A firefighter problem instance
@@ -125,7 +127,6 @@ pub struct OSMFProblem {
     strategy: OSMFStrategy,
     node_data: NodeDataStorage,
     global_time: TimeUnit,
-    change_tracker: BTreeMap<TimeUnit, Vec<usize>>,
     is_active: bool,
 }
 
@@ -143,14 +144,13 @@ impl OSMFProblem {
             strategy,
             node_data: NodeDataStorage::new(),
             global_time: 0,
-            change_tracker: BTreeMap::new(),
             is_active: true,
         };
 
         problem.gen_fire_roots();
 
         if let OSMFStrategy::ShortestDistance(ref mut sho_dist_strategy) = problem.strategy {
-            let roots = problem.change_tracker.get(&0).unwrap();
+            let roots = problem.node_data.times.get(&0).unwrap();
             sho_dist_strategy.compute_shortest_dists(roots);
         }
 
@@ -164,30 +164,12 @@ impl OSMFProblem {
         let num_nodes = self.graph.read().unwrap().num_nodes;
         while roots.len() < self.settings.num_roots {
             let root = rng.gen_range(0..num_nodes);
-            if !self.node_data.is_node_data_attached(&root) {
-                self.node_data.attach_node_data(root, NodeState::Burning, self.global_time);
+            if self.node_data.is_undefended(&root) {
                 roots.push(root);
-
-                log::debug!("Set vertex {} as fire root", root);
             }
         }
-        self.track_changes(roots);
-    }
-
-    /// Track a list of changed nodes.
-    /// The changes will be attached to the current global time.
-    fn track_changes(&mut self, changed: Vec<usize>) {
-        match self.change_tracker.get_mut(&self.global_time) {
-            Some(changes) => {
-                changes.reserve_exact(changed.len());
-                for node_id in changed {
-                    changes.push(node_id);
-                }
-            }
-            None => {
-                self.change_tracker.insert(self.global_time, changed);
-            }
-        }
+        log::debug!("Setting nodes {:?} as fire roots", &roots);
+        self.node_data.mark_burning(roots, self.global_time);
     }
 
     /// Spread the fire to all nodes that are adjacent to burning nodes.
@@ -199,7 +181,7 @@ impl OSMFProblem {
 
         let mut to_burn = Vec::new();
         {
-            let burning = self.node_data.get_all_burning();
+            let burning = self.node_data.get_burning();
 
             let graph = self.graph.read().unwrap();
             let offsets = &graph.offsets;
@@ -212,7 +194,7 @@ impl OSMFProblem {
                 let node_id = node_data.node_id;
                 for i in offsets[node_id]..offsets[node_id + 1] {
                     let edge = &edges[i];
-                    if !self.node_data.is_node_data_attached(&edge.tgt) {
+                    if self.node_data.is_undefended(&edge.tgt) {
                         // There is at least one node to be burned at some point in the future
                         if !self.is_active {
                             self.is_active = true;
@@ -228,12 +210,8 @@ impl OSMFProblem {
         }
 
         // Burn all nodes in `to_burn`
-        for node_id in &to_burn {
-            self.node_data.attach_node_data(*node_id, NodeState::Burning, self.global_time);
-
-            log::debug!("Node {} caught fire", node_id);
-        }
-        self.track_changes(to_burn);
+        log::debug!("Burning nodes {:?} ", &to_burn);
+        self.node_data.mark_burning(to_burn, self.global_time);
     }
 
     /// Execute the containment strategy to prevent as much nodes as
@@ -246,14 +224,13 @@ impl OSMFProblem {
         let defended = match self.strategy {
             OSMFStrategy::Greedy(ref mut greedy_strategy) =>
                 greedy_strategy.execute(&self.settings, &mut self.node_data, self.global_time),
-            _ => Vec::default()
+            _ => 0
         };
 
-        if defended.is_empty()
+        if defended == 0
             && matches!(self.strategy, OSMFStrategy::Greedy( .. )) { // TODO remove when shortest distance strategy is implemented
             self.is_active = false;
         }
-        self.track_changes(defended);
     }
 
     /// Execute one time step in the firefighter problem.
@@ -275,40 +252,30 @@ impl OSMFProblem {
 
     /// Generate the simulation response for this firefighter problem instance
     pub fn simulation_response(&self) -> OSMFSimulationResponse {
-        let mut nodes_burned = 0;
-        let mut nodes_defended = 0;
-        for nd in self.node_data.storage.values() {
-            if nd.is_burning() {
-                nodes_burned += 1;
-            } else {
-                nodes_defended += 1;
-            }
-        }
-
         OSMFSimulationResponse {
-            node_data: &self.node_data.storage,
-            nodes_burned,
-            nodes_defended,
+            nodes_burned: self.node_data.burning.len(),
+            nodes_defended: self.node_data.defended.len(),
             nodes_total: self.graph.read().unwrap().num_nodes,
             end_time: self.global_time,
+            node_data: &self.node_data,
         }
     }
 }
 
-#[derive(Debug)]
-pub enum OSMFProblemError {
-    NodeDataAlreadyAttached,
-}
-
-impl std::fmt::Display for OSMFProblemError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::NodeDataAlreadyAttached => write!(f, "Node data is already attached to this node")
-        }
-    }
-}
-
-impl std::error::Error for OSMFProblemError {}
+// #[derive(Debug)]
+// pub enum OSMFProblemError {
+//     NodeDataAlreadyAttached,
+// }
+//
+// impl std::fmt::Display for OSMFProblemError {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Self::NodeDataAlreadyAttached => write!(f, "Node data is already attached to this node")
+//         }
+//     }
+// }
+//
+// impl std::error::Error for OSMFProblemError {}
 
 #[cfg(test)]
 mod test {
@@ -330,16 +297,16 @@ mod test {
         let mut problem = OSMFProblem::new(
             graph.clone(), OSMFSettings::new(num_roots, 2), strategy);
 
-        assert_eq!(problem.node_data.storage.len(), num_roots);
-        assert_eq!(problem.change_tracker.len(), (problem.global_time + 1) as usize);
-        assert_eq!(problem.change_tracker[&problem.global_time].len(), num_roots);
+        assert_eq!(problem.node_data.burning.len(), num_roots);
+        assert_eq!(problem.node_data.times.len(), (problem.global_time + 1) as usize);
+        assert_eq!(problem.node_data.times.get(&problem.global_time).unwrap().len(), num_roots);
 
         let graph_ = graph.read().unwrap();
         let num_nodes = graph_.num_nodes;
 
         let roots: Vec<_>;
         {
-            roots = problem.node_data.storage.keys()
+            roots = problem.node_data.burning.keys()
                 .into_iter()
                 .map(|k| *k)
                 .collect();
@@ -365,7 +332,7 @@ mod test {
 
         problem.exec_step();
 
-        assert_eq!(problem.change_tracker.len(), (problem.global_time + 1) as usize);
+        assert_eq!(problem.node_data.times.len(), (problem.global_time + 1) as usize);
 
         let mut targets = Vec::new();
         let mut distances = BTreeMap::new();
@@ -375,20 +342,19 @@ mod test {
             for i in graph_.offsets[*root]..graph_.offsets[*root + 1] {
                 let edge = &graph_.edges[i];
                 targets.push(edge.tgt);
-                distances.insert(edge.tgt, edge.dist);
+                distances.insert(edge.tgt, edge.dist as u64);
             }
         }
 
-        for node_id in &problem.change_tracker[&problem.global_time] {
+        for node_id in problem.node_data.times.get(&problem.global_time).unwrap() {
             assert!(targets.contains(node_id));
         }
 
         for root in &roots {
-            let root_nd = problem.node_data.storage.get(root).unwrap();
+            let root_nd = problem.node_data.burning.get(root).unwrap();
             for tgt in &targets {
-                match problem.node_data.storage.get(tgt) {
-                    Some(nd) => assert!(nd.is_burning()),
-                    None => assert!(problem.global_time < root_nd.time + distances[tgt] as u64)
+                if problem.node_data.is_undefended(tgt) {
+                    assert!(problem.global_time < root_nd.time + *distances.get(tgt).unwrap())
                 }
             }
         }
