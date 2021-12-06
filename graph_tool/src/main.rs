a81b1745cebf4f30e0802f73151201b56cbd3b81
Aimn Ahmed <st150637@stud.uni-stuttgart.de>
2021-12-06 16:25:36 +0100

    cleaned code: deleted unused imports and code, fixed warnings

--- graph_tool/src/main.rs
+++ graph_tool/src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::{BTreeMap, HashMap};
-use std::{env, fs};
+use std::collections::BTreeMap;
+use std::env;
 use std::fmt::Formatter;
 use std::fs::File;
 use std::io::{BufRead, BufReader, LineWriter, Write};
@@ -10,7 +10,7 @@ enum ParseError {
     IO(std::io::Error),
     ParseInt(ParseIntError),
     ParseFloat(ParseFloatError),
-    InvalidNode(usize),
+    //InvalidNode(usize),
 }
 
 impl std::fmt::Display for ParseError {
@@ -19,7 +19,7 @@ impl std::fmt::Display for ParseError {
             Self::IO(err) => write!(f, "{}", err.to_string()),
             Self::ParseInt(err) => write!(f, "{}", err.to_string()),
             Self::ParseFloat(err) => write!(f, "{}", err.to_string()),
-            Self::InvalidNode(node_id) => write!(f, "Invalid node {}", node_id)
+            //Self::InvalidNode(node_id) => write!(f, "Invalid node {}", node_id)
         }
     }
 }
@@ -30,7 +30,7 @@ impl std::error::Error for ParseError {
             Self::IO(ref err) => Some(err),
             Self::ParseInt(ref err) => Some(err),
             Self::ParseFloat(ref err) => Some(err),
-            _ => None
+            //_ => None
         }
     }
 }
@@ -54,7 +54,6 @@ impl From<ParseFloatError> for ParseError {
 }
 
 /// An undirected graph edge between two nodes a and b
-//#[derive(Debug, Serialize, Default)]
 pub struct Edge {
     pub a: usize,
     pub b: usize,
@@ -64,26 +63,20 @@ pub struct Edge {
 }
 
 /// A graph node with id, latitude and longitude
-//#[derive(Debug, Serialize, Default)]
 pub struct Node {
     pub id: usize,
     pub id2: usize,
     pub lat: String,
     pub lon: String,
     pub elevation: String,
-    //bwd_hubs: Vec<HubLabel>,
-    //fwd_hubs: Vec<HubLabel>,
 }
 
 /// An undirected graph with nodes and edges
-//#[derive(Debug, Serialize, Default)]
 pub struct Graph {
     pub meta: String,
     pub nodes: Vec<Node>,
     pub edges: Vec<Edge>,
     pub new_edges: Vec<(usize, usize, usize, String, String)>,
-    //pub offsets: Vec<usize>,
-    //pub in_degrees: Vec<usize>,
     pub num_nodes: usize,
     pub num_edges: usize,
     pub new_num_edges: usize,
@@ -126,7 +119,6 @@ impl Graph {
             let mut split = line.split(" ");
             line_no += 1;
             split.next(); // id
-            //split.next(); // second id
 
             let node = Node {
                 id: i,
@@ -146,17 +138,10 @@ impl Graph {
                     .expect(&format!("Unexpected EOL while parsing node latitude in line {}",
                                      line_no))
                     .to_string(),
-                //bwd_hubs: Vec::new(),
-                //fwd_hubs: Vec::new(),
             };
             self.nodes.push(node);
         }
-
-        //let mut last_src: i64 = -1;
-        //let mut offset: usize = 0;
         self.edges.reserve_exact(self.num_edges);
-        //self.offsets.resize(self.num_nodes + 1, 0);
-        //self.in_degrees.resize(self.num_nodes, 0);
 
         let mut new_temp_edges: BTreeMap<(usize, usize), Vec<(usize, usize, usize, String, String)>> = BTreeMap::new();
         for _ in 0..self.num_edges {
@@ -207,20 +192,6 @@ impl Graph {
                     })
                     .or_insert(vec![(edge.a, edge.b, edge.dist, edge.edge_type.clone(), edge.maxspeed.clone()), (edge.b, edge.a, edge.dist, edge.edge_type, edge.maxspeed)]);
             }
-
-            /*
-            if edge.src as i64 > last_src {
-                for j in (last_src + 1) as usize..=edge.src {
-                    self.offsets[j] = offset;
-                }
-                last_src = edge.src as i64;
-            }
-            offset += 1;
-
-            self.in_degrees[edge.tgt] += 1;
-            self.edges.push(edge);
-
-            */
         }
 
         for edge_values in new_temp_edges.values() {
@@ -238,7 +209,6 @@ impl Graph {
             })
         });
         self.new_num_edges = self.new_edges.len();
-        //self.offsets[self.num_nodes] = self.num_edges;
         Ok(())
     }
 
@@ -251,7 +221,7 @@ impl Graph {
         file.write((format!("{}\n", self.new_num_edges)).as_bytes())?;
 
         for node in &self.nodes {
-            file.write((format!("{} {} {} {} {}\n", node.id, node.id2, node.lat, node.lon, node.elevation)).as_bytes());
+            file.write((format!("{} {} {} {} {}\n", node.id, node.id2, node.lat, node.lon, node.elevation)).as_bytes())?;
         }
 
         for (a, b, dist, edge_type, maxspeed) in &self.new_edges {
@@ -267,13 +237,11 @@ fn main() -> Result<(), ParseError> {
     // Initialize logger
     env::set_var("RUST_LOG", "debug");
     env::set_var("RUST_BACKTRACE", "1");
-    //env_logger::init();
 
     let args: Vec<_> = env::args().collect();
 
     if args.len() < 3 {
         let err = "Missing argument: path to new undirected graph file";
-        //log::error!("{}", err);
         panic!("{}", err);
     }
 
@@ -290,7 +258,6 @@ fn main() -> Result<(), ParseError> {
         new_num_edges: 0
     };
     graph.parse_graph(&in_graph)?;
-    graph.write_graph(&out_graph);
-
+    graph.write_graph(&out_graph)?;
     Ok(())
 }
