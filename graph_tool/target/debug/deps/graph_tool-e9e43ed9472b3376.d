7970a1b92faf7a85b8073d061f18323a57c00503
Aimn Ahmed <st150637@stud.uni-stuttgart.de>
2021-12-06 16:18:21 +0100

    gitignore for graph tool

--- graph_tool/target/debug/deps/graph_tool-e9e43ed9472b3376.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Acer\Documents\Projektarbeit Softwaretechnik\OSM-Firefighter\graph_tool\target\debug\deps\graph_tool-e9e43ed9472b3376.exe: src\main.rs
-
-C:\Users\Acer\Documents\Projektarbeit Softwaretechnik\OSM-Firefighter\graph_tool\target\debug\deps\graph_tool-e9e43ed9472b3376.d: src\main.rs
-
-src\main.rs:
--- graph_tool/target/debug/graph_tool.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Acer\Documents\Projektarbeit\ Softwaretechnik\OSM-Firefighter\graph_tool\target\debug\graph_tool.exe: C:\Users\Acer\Documents\Projektarbeit\ Softwaretechnik\OSM-Firefighter\graph_tool\src\main.rs
